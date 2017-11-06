bb9786913ea115e4b34e6660a6ef87dd38ec167f
Rahul Sahay <rahulsahay19@hotmail.com>
2017-11-06 14:33:40 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  constructor(private router: Router){
+  
+  }
+  ngOnInit() {
+    //This is mimicking service call in a sense say REST Call
+    setTimeout(()=>{
+        this.router.navigate(['/lazy/load-movie']);
+        },3000);
+  
+  }
+
   title = 'Lazy Load Demo!';
 }
