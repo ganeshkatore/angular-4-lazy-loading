2aa43e8648d86c7e179dfb81c4faecf36904bdb0
Rahul Sahay <rahulsahay19@hotmail.com>
2017-11-06 14:38:24 +0100

    comment added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//This part needs to be dynamic. Let's say, I get the configuration from REST Call here
+//and then below, it will get substituted back to routes stuff, which means it basically 
+//becomes collection of loadChildrens. But, this will get activated once it gets visited.
+
+//If we want activate in one short, then that part should be placed in eager loading like
+//a normal way.
 const routes: Routes = [
   { path: 'lazy', loadChildren: './lazy.module#LazyModule'}
 ];
