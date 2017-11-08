762f416e5bd161fe4d2af62458b9d738e1c49e57
Rahul Sahay <rahulsahay19@hotmail.com>
2017-11-08 14:15:34 +0100

    updated one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent  {
    
   title = 'Lazy Loading Module!';
   name= 'Rahul Sahay';
-  print() {
+  reverse() {
     import('./print-output').then(module => {
       this.name = module.printMessage(this.name);
     });
