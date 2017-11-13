f92ddbacba51c1002c9bfc4b0ea981da90328f40
Rahul Sahay <rahulsahay19@hotmail.com>
2017-11-13 16:19:23 +0100

    commiting package.json file

--- src/app/index.ts
+++ src/app/index.ts
@@ -12,9 +12,12 @@ export * from "../polyfills";
 export * from "../main";
 export * from "./dynamic.module";
 export * from "./dynamic.component";
+export * from "./dynamic-routing.module";
 export * from "./movies/Movie";
 export * from "./movies/movies.component";
 export * from "./movies/movies.service";
+export * from "./inline.component";
+export * from "./looging-service";
 
 
   
